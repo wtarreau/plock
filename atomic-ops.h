4b0dbfe6df7bc1957f14c341bfc010223138a958
Willy Tarreau <w@1wt.eu>
2017-03-05 15:12:18 +0100

    atomic-ops: split ix86 and x86_64
    Some immediate instructions require a size suffix (addq, subq, andq)
    which differs between 32 and 64 bit operands and which cannot be
    determined by the assembler. In addition, immediate operands on x86_64
    require the "e" constraint instead of "i" because it is not possible
    to pass values larger than 32 bits. Thus better split the code between
    the two arches.
    
    While doing so, the unused atomic_add64 and atomic_inc64 functions
    were removed since their original purpose was to implement the "long"
    variants of the operations while not applicable to 32-bit.

--- atomic-ops.h
+++ atomic-ops.h
@@ -1,6 +1,9 @@
 #ifndef ATOMIC_OPS_H
 #define ATOMIC_OPS_H
 
+#if defined(__i386__) || defined (__i486__) || defined (__i586__) || defined (__i686__)
+
+/* x86 32 bits */
 
 static inline void cpu_relax()
 {
@@ -18,7 +21,7 @@ static inline void cpu_relax_long(unsigned long cycles)
 static inline unsigned char atomic_inc(volatile unsigned long *ptr)
 {
 	unsigned char ret;
-	asm volatile("lock inc %0\n"
+	asm volatile("lock incl %0\n"
 		     "setne %1\n"
 		     : "+m" (*ptr), "=qm" (ret)
 		     :
@@ -30,7 +33,7 @@ static inline unsigned char atomic_inc(volatile unsigned long *ptr)
 static inline unsigned char atomic_dec(volatile unsigned long *ptr)
 {
 	unsigned char ret;
-	asm volatile("lock dec %0\n"
+	asm volatile("lock decl %0\n"
 		     "setne %1\n"
 		     : "+m" (*ptr), "=qm" (ret)
 		     :
@@ -41,19 +44,19 @@ static inline unsigned char atomic_dec(volatile unsigned long *ptr)
 /* increment bit value */
 static inline void atomic_inc_noret(volatile unsigned long *ptr)
 {
-	asm volatile("lock inc %0\n" : "+m" (*ptr) :: "memory");
+	asm volatile("lock incl %0\n" : "+m" (*ptr) :: "memory");
 }
 
 /* decrement bit value */
 static inline void atomic_dec_noret(volatile unsigned long *ptr)
 {
-	asm volatile("lock dec %0\n" : "+m" (*ptr) :: "memory");
+	asm volatile("lock decl %0\n" : "+m" (*ptr) :: "memory");
 }
 
 /* cost for 100M ops on P-M 1.7: .36s without lock, 1.32s with lock ! */
 static inline void atomic_add(volatile unsigned long *ptr, unsigned long x)
 {
-	asm volatile("lock add %1, %0\n"
+	asm volatile("lock addl %1, %0\n"
 		     : "+m" (*ptr)
 		     : "ir" (x)
 		     : "memory");
@@ -62,7 +65,7 @@ static inline void atomic_add(volatile unsigned long *ptr, unsigned long x)
 /* cost for 100M ops on P-M 1.7: .36s without lock, 1.32s with lock ! */
 static inline void atomic_sub(volatile unsigned long *ptr, unsigned long x)
 {
-	asm volatile("lock sub %1, %0\n"
+	asm volatile("lock subl %1, %0\n"
 		     : "+m" (*ptr)
 		     : "ir" (x)
 		     : "memory");
@@ -71,7 +74,7 @@ static inline void atomic_sub(volatile unsigned long *ptr, unsigned long x)
 /* cost for 100M ops on P-M 1.7: .36s without lock, 1.32s with lock ! */
 static inline void atomic_and(volatile unsigned long *ptr, unsigned long x)
 {
-	asm volatile("lock and %1, %0\n"
+	asm volatile("lock andl %1, %0\n"
 		     : "+m" (*ptr)
 		     : "ir" (x)
 		     : "memory");
@@ -81,7 +84,7 @@ static inline void atomic_and(volatile unsigned long *ptr, unsigned long x)
 static inline unsigned long atomic_bts(volatile unsigned long *ptr, const unsigned long bit)
 {
 	unsigned long ret;
-	asm volatile("lock bts %2, %0\n\t"
+	asm volatile("lock btsl %2, %0\n\t"
 		     "sbb %1, %1\n\t"
 		     : "+m" (*ptr), "=r" (ret)
 		     : "Ir" (bit)
@@ -92,7 +95,7 @@ static inline unsigned long atomic_bts(volatile unsigned long *ptr, const unsign
 /* temp = x; x = *ptr ; *ptr += temp */
 static inline unsigned long xadd(volatile unsigned long *ptr, unsigned long x)
 {
-	asm volatile("lock xadd %0, %1\n"
+	asm volatile("lock xaddl %0, %1\n"
 		     :  "=r" (x), "+m" (*ptr)
 		     : "0" (x)
 		     : "memory");
@@ -120,26 +123,126 @@ static inline unsigned long cmpxchg(volatile unsigned long *ptr, unsigned long o
 	return ret;
 }
 
-/* increment 64-bit value */
-static inline void atomic_inc64_noret(volatile unsigned long long *ptr)
+
+#elif defined(__x86_64__)
+/* x86 64 bits */
+
+static inline void cpu_relax()
 {
-	asm volatile("lock inc %0\n" : "+m" (*ptr) :: "memory");
+	asm volatile("rep;nop\n" ::: "memory");
 }
 
-/* decrement 64-bit value */
-static inline void atomic_dec64_noret(volatile unsigned long long *ptr)
+static inline void cpu_relax_long(unsigned long cycles)
 {
-	asm volatile("lock dec %0\n" : "+m" (*ptr) :: "memory");
+	do {
+		cpu_relax();
+	} while (--cycles);
 }
 
-static inline void atomic_add64(volatile unsigned long long *ptr, unsigned long x)
+/* increment value and return non-zero if result is non-null */
+static inline unsigned char atomic_inc(volatile unsigned long *ptr)
 {
-	asm volatile("lock add %1, %0\n"
+	unsigned char ret;
+	asm volatile("lock incq %0\n"
+		     "setne %1\n"
+		     : "+m" (*ptr), "=qm" (ret)
+		     :
+		     : "memory");
+	return ret;
+}
+
+/* decrement value and return non-zero if result is non-null */
+static inline unsigned char atomic_dec(volatile unsigned long *ptr)
+{
+	unsigned char ret;
+	asm volatile("lock decq %0\n"
+		     "setne %1\n"
+		     : "+m" (*ptr), "=qm" (ret)
+		     :
+		     : "memory");
+	return ret;
+}
+
+/* increment bit value */
+static inline void atomic_inc_noret(volatile unsigned long *ptr)
+{
+	asm volatile("lock incq %0\n" : "+m" (*ptr) :: "memory");
+}
+
+/* decrement bit value */
+static inline void atomic_dec_noret(volatile unsigned long *ptr)
+{
+	asm volatile("lock decq %0\n" : "+m" (*ptr) :: "memory");
+}
+
+static inline void atomic_add(volatile unsigned long *ptr, unsigned long x)
+{
+	asm volatile("lock addq %1, %0\n"
 		     : "+m" (*ptr)
-		     : "ir" (x)
+		     : "er" (x)
 		     : "memory");
 }
 
+static inline void atomic_sub(volatile unsigned long *ptr, unsigned long x)
+{
+	asm volatile("lock subq %1, %0\n"
+		     : "+m" (*ptr)
+		     : "er" (x)
+		     : "memory");
+}
+
+static inline void atomic_and(volatile unsigned long *ptr, unsigned long x)
+{
+	asm volatile("lock andq %1, %0\n"
+		     : "+m" (*ptr)
+		     : "er" (x)
+		     : "memory");
+}
+
+/* test and set a bit. Previous value is returned as 0 (not set) or -1 (set). */
+static inline unsigned long atomic_bts(volatile unsigned long *ptr, const unsigned long bit)
+{
+	unsigned long ret;
+	asm volatile("lock btsq %2, %0\n\t"
+		     "sbb %1, %1\n\t"
+		     : "+m" (*ptr), "=r" (ret)
+		     : "Ir" (bit)
+		     : "memory");
+	return ret;
+}
+
+/* temp = x; x = *ptr ; *ptr += temp */
+static inline unsigned long xadd(volatile unsigned long *ptr, unsigned long x)
+{
+	asm volatile("lock xaddq %0, %1\n"
+		     :  "=r" (x), "+m" (*ptr)
+		     : "0" (x)
+		     : "memory");
+	return x;
+}
+
+/* exchange <x> with <*ptr> and return previous contents of <*ptr> */
+static inline unsigned long xchg(volatile unsigned long *ptr, unsigned long x)
+{
+	asm volatile("lock xchg %0,%1"
+		     : "=r" (x), "+m" (*ptr)
+		     : "0" (x)
+		     : "memory");
+	return x;
+}
+
+static inline unsigned long cmpxchg(volatile unsigned long *ptr, unsigned long old, unsigned long new)
+{
+	unsigned long ret;
+
+	asm volatile("lock cmpxchg %2,%1"
+		     : "=a" (ret), "+m" (*ptr)
+		     : "r" (new), "0" (old)
+		     : "memory");
+	return ret;
+}
+
+#endif
 /* TODO: atomic ops on pointers (eg: lock xadd without length suffix) */
 
 #endif /* ATOMIC_OPS_H */
